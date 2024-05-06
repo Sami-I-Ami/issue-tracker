bd0632aa8930ddb753abe81884addf6b6666a50b
Sami Fink <fink.s.m.8651@gmail.com>
2024-05-06 20:38:46 +0000

    feat: added error for not entering required field

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,9 @@
 
 module.exports = function (app) {
 
+  let currentId = 0;
+  let issues = [];
+
   app.route('/api/issues/:project')
   
     .get(function (req, res){
@@ -11,6 +14,12 @@ module.exports = function (app) {
     
     .post(function (req, res){
       let project = req.params.project;
+      let { issue_title, issue_text, created_by, assigned_to, status_text } = req.body;
+      if (!issue_title.trim() | !issue_text.trim() | !created_by.trim()) {
+        res.json({error: 'required field(s) missing'});
+      } else {
+        
+      }
       
     })
     
