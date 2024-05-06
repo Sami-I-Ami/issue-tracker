77b0583c64a20f424a9a2ec89e398649791b1825
Sami Fink <fink.s.m.8651@gmail.com>
2024-05-06 20:55:01 +0000

    feat: created response for valid post request

--- routes/api.js
+++ routes/api.js
@@ -18,9 +18,27 @@ module.exports = function (app) {
       if (!issue_title.trim() | !issue_text.trim() | !created_by.trim()) {
         res.json({error: 'required field(s) missing'});
       } else {
-        
+        currentId++;
+        if (!assigned_to) {
+          assigned_to = "";
+        }
+        if (!status_text) {
+          status_text = "";
+        }
+        const issueObj = {
+          _id: currentId,
+          issue_title: issue_title,
+          issue_text: issue_text,
+          created_on: new Date(),
+          updated_on: new Date(),
+          created_by: created_by,
+          assigned_to: assigned_to,
+          open: true,
+          status_text: status_text
+        };
+        issues.push({project_name: project, issueObj})
+        res.json(issueObj);
       }
-      
     })
     
     .put(function (req, res){
